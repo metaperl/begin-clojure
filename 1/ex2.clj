de6d8ec266b1af882c59b4ae1b9a3078584851e7
unknown <metaperl@gmail.com>
2009-02-05 02:51:59 -0500

    chapter 1 done

---
+++ 1/ex2.clj
@@ -0,0 +1,5 @@
+; (. System getProperty "line.separator")  -> "\r\n"
+
+(. javax.swing.JOptionPane (showMessageDialog nil "Hello World\r\nHi Mom"))
+
+
---
+++ 1/unicode-chars.clj
@@ -0,0 +1,5 @@
+(. javax.swing.JOptionPane (showMessageDialog nil (str (char 0x2620))))
+(. javax.swing.JOptionPane (showMessageDialog nil (str (char 0x2620) (char 0x1F18) (char 0x0950))))
+
+(apply str (map char (range 100 150)))
+
